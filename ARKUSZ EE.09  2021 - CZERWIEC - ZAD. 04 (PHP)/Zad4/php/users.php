9bc7f72f86edc31f98187307fcf2c4e2ff1dce74
FoxLys <FoxLys.com@gmail.com>
2024-06-23 16:46:10 +0200

    Add files via upload

---
+++ ARKUSZ EE.09  2021 - CZERWIEC - ZAD. 04 (PHP)/Zad4/php/users.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styl4.css" type="css">
+    <title>Panel administratora</title>
+</head>
+<body>
+    <div class="baner">
+        <h3>Portal Społecznościowy - panel administratora</h3>
+    </div>
+    <div class="lewy">
+
+    </div>
+    <div class="prawy">
+
+    </div>
+    <div class="stopka">
+
+    </div>
+</body>
+</html>
